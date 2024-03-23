08278c8ba711454bdba73467cd497b0655b4c2ac
Brayan-has <g.nayarb112@gmail.com>
2024-03-23 14:05:50 -0500

    fix: arreglar error perdon .c

--- backEnd/index.php
+++ backEnd/index.php
@@ -1,5 +1,14 @@
 <?php
 
-echo "Hola mundo";
+
+$nombre = $_POST['usuario'];
+$contrasena = $_POST['contrasena'];
+
+
+if ($nombre == "Juan" and $contrasena == "123") {
+    echo "Yo soy " . $nombre . " y este es mi cuenta!";
+} else {
+    header("Location: ../login/registro.html");
+}
 
 ?>
