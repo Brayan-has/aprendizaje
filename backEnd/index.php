b587de3060c464df63fa066348a4c4c30be9c1b3
Brayan-has <g.nayarb112@gmail.com>
2024-03-23 16:57:25 -0500

    fix: error keep

---
+++ backEnd/index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$nombre = $_POST['usuario'];
+$contrasena = $_POST['contrasena'];
+
+
+if ($nombre == "Juan" and $contrasena == "123") {
+    echo "Yo soy " . $nombre . " y este es mi cuenta!";
+} else {
+    header("Location: ../login/registro.html");
+}
+
+if($nombre == "" || $contrasena == ""){
+    header("Location: ../index..html");
+}
+?>
