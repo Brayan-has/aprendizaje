57dea32319ca4f6f25c73c1dbd39e07bb2323fc8
Brayan-has <g.nayarb112@gmail.com>
2024-03-23 17:04:54 -0500

    feat: fix

--- backEnd/index.php
+++ backEnd/index.php
@@ -11,21 +11,3 @@ if ($nombre == "Juan" and $contrasena == "123") {
     header("Location: ../login/registro.html");
 }
 
-if($nombre == "" || $contrasena == ""){
-    header("Location: ../index..html");
-}
-?>
-<?php
-
-
-$nombre = $_POST['usuario'];
-$contrasena = $_POST['contrasena'];
-
-
-if ($nombre == "Juan" and $contrasena == "123") {
-    echo "Yo soy " . $nombre . " y este es mi cuenta!";
-} else {
-    header("Location: ../login/registro.html");
-}
-
-?>
